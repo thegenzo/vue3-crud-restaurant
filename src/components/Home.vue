c5d5fd597d785b1d375cfd814de6f739dedca7dd
Dicky Agusputra <loscarbulos@gmail.com>
2022-10-17 13:49:17 +0800

    logout testing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
 	<h1>Hello user, welcome on home page</h1>
+	<button type="button" @click="logout" onclick="return confirm('are you sure to logout?')">Logout test</button>
 </template>
 
 <script>
@@ -9,6 +10,17 @@
 			let user = localStorage.getItem('user-info')
 			if (!user) {
 				this.$router.push({name: 'SignUp'})
+			}
+		},
+		methods: {
+			async logout() {
+				let result = await localStorage.getItem('user-info')
+				console.log(result)
+
+				if (result) {
+					localStorage.removeItem('user-info')
+					this.$router.push('/login')
+				}
 			}
 		}
 	}

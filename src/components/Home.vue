383946ac57dc2dd8babc45dd56725defcb54d1bb
thegenzo <loscarbulos@gmail.com>
2022-10-18 10:01:06 +0800

    lesson 15
    - call API restaurants
    - make list of table from API restaurants

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,26 @@
 <template>
 	<Header />
 	<h1>Hello {{ name }}, welcome on home page</h1>
+	<table border="1px">
+		<tr>
+			<td>ID</td>
+			<td>Name</td>
+			<td>Contact</td>
+			<td>Address</td>
+		</tr>
+		<tr v-for="item in restaurant" :key="item.id">
+			<td>{{ item.id }}</td>
+			<td>{{ item.name }}</td>
+			<td>{{ item.contact }}</td>
+			<td>{{ item.address }}</td>
+		</tr>
+	</table>
 </template>
 
 <script>
 import Header from './Header.vue'
+import axios from 'axios'
+
 export default {
 	name: 'home-page',
 	components: {
@@ -12,15 +28,27 @@ export default {
 	},
 	data() {
 		return {
-			name: ''
+			name: '',
+			restaurant: []
 		}
 	},
-	mounted() {
-		let user = localStorage.getItem('user-info')
-		this.name = JSON.parse(user).name
+	async mounted() {
+		let user =  localStorage.getItem('user-info')
+		this.name = await JSON.parse(user).name
 		if (!user) {
 			this.$router.push({name: 'SignUp'})
 		}
+
+		let result = await axios.get('http://localhost:3000/restaurants')
+		this.restaurant = result.data
+
 	},
 }
-</script>
+</script> 
+
+<style>
+td {
+	width: 160px;
+	height: 40px;
+}
+</style>

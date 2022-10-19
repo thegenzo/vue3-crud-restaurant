0d22edf67f7dc40d7ae5b3712270125d89ada7af
thegenzo <loscarbulos@gmail.com>
2022-10-19 09:21:54 +0800

    lesson 16
    - create add form
    - implement two way data bindings
    - style add form
    - print add result on console warn

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ body {
   width: 100px;
 }
 
-.register input, .login input {
+.register input, .login input, .add input {
   width: 300px;
   height: 40px;
   padding-left: 20px;
@@ -37,7 +37,7 @@ body {
   border: 1px solid skyblue;
 }
 
-.register button, .login button {
+.register button, .login button, .add button {
   width: 320px;
   height: 40px;
   border: 1px solid skyblue;
--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,12 @@
 <template>
 	<Header />
 	<h1>Hello user, welcome on add restaurant page</h1>
+	<form action="" class="add">
+		<input type="text" name="name" id="name" placeholder="enter name" v-model="restaurant.name">
+		<input type="text" name="address" id="address" placeholder="enter address" v-model="restaurant.address">
+		<input type="text" name="contact" id="contact" placeholder="enter contact" v-model="restaurant.contact">
+		<button type="button" @click="addRestaurant">add new restaurant</button>
+	</form>
 </template>
 
 <script>
@@ -9,6 +15,20 @@ export default {
 	name: 'add-page',
 	components: {
 		Header
+	},
+	data() {
+		return {
+			restaurant: {
+				name: '',
+				address: '',
+				contact: ''
+			}
+		}
+	},
+	methods: {
+		addRestaurant() {
+			console.warn(this.restaurant)
+		}
 	},
 	mounted() {
 		let user = localStorage.getItem('user-info')
